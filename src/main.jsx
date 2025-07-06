4b891f7238875b4e794259e4f1e2dc8b4508ad4f
fisheeesh <swanphyo444@gmail.com>
2025-07-07 03:49:05 +0700

    fix: fixed import path typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './router/index.jsx'
-import { reportWebVitals } from './ReportWebVitals.js';
+import { reportWebVitals } from './reportWebVitals.js';
 
 createRoot(document.getElementById('root')).render(
   <Router />
